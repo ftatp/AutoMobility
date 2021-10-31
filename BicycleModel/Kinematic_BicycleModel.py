4f50e9253c961c8d6f917742dce3428c745b62c9
ftatp <ftatp5901@gmail.com>
2021-10-31 18:25:32 +0900

    Created Bicycle class

---
+++ BicycleModel/Kinematic_BicycleModel.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+class Bicycle():
+    def __init__(self):
+        self.xc = 0
+        self.yc = 0
+        self.theta = 0
+        self.delta = 0
+        self.beta = 0
+        
+        self.L = 2
+        self.lr = 1.2
+        self.w_max = 1.22
+        
+        self.sample_time = 0.01
+        
+    def reset(self):
+        self.xc = 0
+        self.yc = 0
+        self.theta = 0
+        self.delta = 0
+        self.beta = 0
