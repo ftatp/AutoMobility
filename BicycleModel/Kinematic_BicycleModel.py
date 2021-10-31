ece06b676d9fecddaac6ff5649850fa8ca077eea
ftatp <ftatp5901@gmail.com>
2021-10-31 19:28:25 +0900

    Set angle liitation and initialize and reach limitation

--- BicycleModel/Kinematic_BicycleModel.py
+++ BicycleModel/Kinematic_BicycleModel.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 class Bicycle():
     def __init__(self):
--- BicycleModel/main.py
+++ BicycleModel/main.py
@@ -23,7 +23,25 @@ for i in range(t_data.shape[0]):
     model.step(np.pi, 0)
     model.beta = 0
 
+#plt.axis('equal')
+#plt.plot(x_data, y_data,label='Learner Model')
+#plt.legend()
+#plt.show()
+#
+
+x_data_new = np.zeros_like(t_data)
+
+for i in range(t_data.shape[0]):
+    x_data_new[i] = model.xc
+    y_data[i] = model.yc
+
+    if model.delta < np.arctan(2/10):
+        model.step(np.pi, 2 * model.w_max)
+    else:
+        model.step(np.pi, 0)
+
 plt.axis('equal')
 plt.plot(x_data, y_data,label='Learner Model')
+plt.plot(x_data_new, y_data,label='Revised Model')
 plt.legend()
 plt.show()
