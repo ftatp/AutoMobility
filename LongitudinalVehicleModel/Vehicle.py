21697a5436d7779642b9866472299f9dff3747cb
ftatp <ftatp5901@gmail.com>
2021-11-01 20:42:21 +0900

    Set throttle / decline and run in steps

--- LongitudinalVehicleModel/Vehicle.py
+++ LongitudinalVehicleModel/Vehicle.py
@@ -74,3 +74,27 @@ class Vehicle():
         self.a = (F_x - F_load) / self.m
         self.v += self.a * self.sample_time
         self.x += (self.v * self.sample_time) - (0.5 * self.a * self.sample_time**2)
+
+
+
+sample_time = 0.01
+time_end = 100
+model = Vehicle()
+
+t_data = np.arange(0, time_end, sample_time)
+a_data = np.zeros_like(t_data)
+v_data = np.zeros_like(t_data)
+
+# throttle percentage between 0 and 1
+throttle = 0.2
+
+# incline angle (in radians)
+alpha = 0
+
+for i in range(t_data.shape[0]):
+    a_data[i] = model.a
+    v_data[i] = model.v
+    model.step(throttle, alpha)
+
+plt.plot(t_data, v_data)
+plt.show()
