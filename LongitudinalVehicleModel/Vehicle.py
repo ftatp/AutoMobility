6a8311533ebe618d290df62cf9386e7588762bf9
ftatp <ftatp5901@gmail.com>
2021-11-01 20:46:16 +0900

    Add main function to Vehicle model

--- LongitudinalVehicleModel/Vehicle.py
+++ LongitudinalVehicleModel/Vehicle.py
@@ -76,25 +76,25 @@ class Vehicle():
         self.x += (self.v * self.sample_time) - (0.5 * self.a * self.sample_time**2)
 
 
+if __name__ == "__main__":
+    sample_time = 0.01
+    time_end = 100
+    model = Vehicle()
 
-sample_time = 0.01
-time_end = 100
-model = Vehicle()
+    t_data = np.arange(0, time_end, sample_time)
+    a_data = np.zeros_like(t_data)
+    v_data = np.zeros_like(t_data)
 
-t_data = np.arange(0, time_end, sample_time)
-a_data = np.zeros_like(t_data)
-v_data = np.zeros_like(t_data)
+    # throttle percentage between 0 and 1
+    throttle = 0.2
 
-# throttle percentage between 0 and 1
-throttle = 0.2
+    # incline angle (in radians)
+    alpha = 0
 
-# incline angle (in radians)
-alpha = 0
+    for i in range(t_data.shape[0]):
+        a_data[i] = model.a
+        v_data[i] = model.v
+        model.step(throttle, alpha)
 
-for i in range(t_data.shape[0]):
-    a_data[i] = model.a
-    v_data[i] = model.v
-    model.step(throttle, alpha)
-
-plt.plot(t_data, v_data)
-plt.show()
+    plt.plot(t_data, v_data)
+    plt.show()
